72a17c6b2163fa1568931be755eda9b45fde3d0a
Oumaima0762 <ouma.hajj123@gmail.com>
2022-11-16 22:31:09 +0100

    button

---
+++ app/src/main/java/com/example/dikr/ActivityLoad.java
@@ -0,0 +1,19 @@
+package com.example.dikr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityload);
+
+
+
+
+
+    }
+}
