19080f2927723b756be47440585af50a5e09b3f5
Oumaima0762 <ouma.hajj123@gmail.com>
2022-11-17 18:01:55 +0100

    login! 2

--- app/src/main/java/com/example/dikr/ActivityLoad.java
+++ app/src/main/java/com/example/dikr/ActivityLoad.java
@@ -2,18 +2,29 @@ package com.example.dikr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ActivityLoad extends AppCompatActivity {
 
+    Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activityload);
 
-
-
-
-
+                Runnable runnable=new Runnable() {
+                    @Override
+                    public void run() {
+                        run_activity();
+                    }
+                };
+                handler.postDelayed(runnable,2*1000);
+    }
+    public  void run_activity(){
+        Intent intent=new Intent(this,ActivityLogin.class);
+        startActivity(intent);
+        finish();
     }
 }
