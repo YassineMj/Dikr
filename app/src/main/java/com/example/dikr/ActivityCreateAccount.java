bddc89073ff60cf72fab5ca22cc164c050354c9f
Yassine <yassinmoujahid46@gmail.com>
2022-11-18 02:21:01 +0100

    new compte

---
+++ app/src/main/java/com/example/dikr/ActivityCreateAccount.java
@@ -0,0 +1,56 @@
+package com.example.dikr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hbb20.CountryCodePicker;
+
+public class ActivityCreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        Button btn_save=findViewById(R.id.btn_save);
+        CountryCodePicker ccp=findViewById(R.id.ccp);
+        EditText txt_first=findViewById(R.id.txt_first);
+        EditText txt_last=findViewById(R.id.txt_last);
+        EditText txt_acc_email=findViewById(R.id.txt_acc_email);
+        EditText txt_acc_password=findViewById(R.id.txt_acc_password);
+        EditText txt_phone_number=findViewById(R.id.txt_phone_number);
+        EditText txt_password_confirmation=findViewById(R.id.txt_password_confirmation);
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txt_first.getText().toString().equals("") || txt_last.getText().toString().equals("") || txt_acc_email.getText().toString().equals("")
+                   || txt_acc_password.getText().toString().equals("") || txt_password_confirmation.getText().toString().equals("") || txt_phone_number.getText().toString().equals("") ){
+
+                    Toast.makeText(ActivityCreateAccount.this,
+                            "Veuillez remplir tous les champs obligatoires", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    if(txt_acc_password.getText().toString().equals(txt_password_confirmation.getText().toString())!=true){
+                        Toast.makeText(ActivityCreateAccount.this,
+                                "Erreur de validation de mot de passe", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        String fullPhoneNumber=(ccp.getSelectedCountryCode().toString()+txt_phone_number.getText().toString());
+                        Toast.makeText(ActivityCreateAccount.this,
+                                "compte créé avec succes", Toast.LENGTH_LONG).show();
+
+                        Toast.makeText(ActivityCreateAccount.this,
+                                "full phone number : "+fullPhoneNumber, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/dikr/ActivityLogin.java
+++ app/src/main/java/com/example/dikr/ActivityLogin.java
@@ -2,7 +2,13 @@ package com.example.dikr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitylogin);
+
+        final Intent IntentCreateAccount=new Intent(this,ActivityCreateAccount.class);
+
+
+        EditText txt_email=findViewById(R.id.txt_email);
+        EditText txt_password=findViewById(R.id.txt_password);
+
+        TextView txt_incription=findViewById(R.id.txt_inscription);
+        Button btn_login=findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txt_email.getText().toString().equals("") || txt_password.getText().toString().equals("")){
+                    Toast.makeText(ActivityLogin.this,
+                            "Veuillez remplir tous les champs obligatoires", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        txt_incription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(IntentCreateAccount);
+            }
+        });
     }
 }
